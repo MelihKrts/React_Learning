cbe72f402014fd764e04d3d2d0f5d3a9b4b45857
MelihKrts <melih.karatas1281@gmail.com>
2025-01-23 22:06:55 +0300

    chore: move all code files to egzersiz folder

---
+++ Egzersizler/State/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+function App() {
+
+    const [count, setCount] = React.useState<number>(0)
+
+    return (
+        <>
+            <div className="bg-[#f5f5f5] w-full container-full mx-auto min-h-screen flex flex-col justify-center items-center">
+                <h1 className="text-3xl font-bold underline"> {count} </h1>
+                <button className="bg-[#3498db] border-amber-100 rounded-sm my-2 p-2 w-32 cursor-pointer"
+                        onClick={() => setCount(count + 1)}>Increase count
+                </button>
+            </div>
+
+        </>
+    )
+}
+
+export default App
