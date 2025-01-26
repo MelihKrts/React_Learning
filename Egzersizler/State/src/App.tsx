b5276bdd6101b3c1bd476e168c87617b7f27163d
MelihKrts <melih.karatas1281@gmail.com>
2025-01-26 14:10:56 +0300

    refactor: improve code formatting and spacing

--- Egzersizler/State/src/App.tsx
+++ Egzersizler/State/src/App.tsx
@@ -6,7 +6,8 @@ function App() {
 
     return (
         <>
-            <div className="bg-[#f5f5f5] w-full container-full mx-auto min-h-screen flex flex-col justify-center items-center">
+            <div
+                className="bg-[#f5f5f5] w-full container-full mx-auto min-h-screen flex flex-col justify-center items-center">
                 <h1 className="text-3xl font-bold underline"> {count} </h1>
                 <button className="bg-[#3498db] border-amber-100 rounded-sm my-2 p-2 w-32 cursor-pointer"
                         onClick={() => setCount(count + 1)}>Increase count
